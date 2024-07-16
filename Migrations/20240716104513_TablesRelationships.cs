f487db1ead18bd6609cba0679fe61bffb80f1ef6
elghaied <elghaied.eslam@gmail.com>
2024-07-16 13:00:28 +0200

    add Migrations files

---
+++ Migrations/20240716104513_TablesRelationships.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TPPizza.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class TablesRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pizzas_Doughs_DoughId",
+                table: "Pizzas");
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    IngredientId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IngredientName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.IngredientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PizzaIngredients",
+                columns: table => new
+                {
+                    PizzaId = table.Column<long>(type: "bigint", nullable: false),
+                    IngredientId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PizzaIngredients", x => new { x.PizzaId, x.IngredientId });
+                    table.ForeignKey(
+                        name: "FK_PizzaIngredients_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "IngredientId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PizzaIngredients_Pizzas_PizzaId",
+                        column: x => x.PizzaId,
+                        principalTable: "Pizzas",
+                        principalColumn: "PizzaId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PizzaIngredients_IngredientId",
+                table: "PizzaIngredients",
+                column: "IngredientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pizzas_Doughs_DoughId",
+                table: "Pizzas",
+                column: "DoughId",
+                principalTable: "Doughs",
+                principalColumn: "DoughId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pizzas_Doughs_DoughId",
+                table: "Pizzas");
+
+            migrationBuilder.DropTable(
+                name: "PizzaIngredients");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pizzas_Doughs_DoughId",
+                table: "Pizzas",
+                column: "DoughId",
+                principalTable: "Doughs",
+                principalColumn: "DoughId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Migrations/20240716105334_AddUniquePizzaName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TPPizza.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniquePizzaName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Pizzas_PizzaName",
+                table: "Pizzas",
+                column: "PizzaName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Pizzas_PizzaName",
+                table: "Pizzas");
+        }
+    }
+}
